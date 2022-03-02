05b56c0df9cae2cb74a7066250674b121c938563
cn1Quan <100780226+cn1Quan@users.noreply.github.com>
2022-03-02 22:54:23 +0800

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,44 +1,40 @@
+def Process():
+  if Option == 0:
+    print("Exiting...")
+    exit()
+  elif Option == 1:
+    First_Number = input("First number: ")
+    Second_Number = input("Second number: ")
+    Answer = int(First_Number) + int(Second_Number)
+    print("Answer: " + str(Answer))
+  elif Option == 2:
+    First_Number = input("First number: ")
+    Second_Number = input("Second number: ")
+    Answer = int(First_Number) - int(Second_Number)
+    print("Answer: " + str(Answer))
+  elif Option == 3:
+    First_Number = input("First number: ")
+    Second_Number = input("Second number: ")
+    Answer = int(First_Number) * int(Second_Number)
+    print("Answer: " + str(Answer))
+  elif Option == 4:
+    First_Number = input("First number: ")
+    Second_Number = input("Second number: ")
+    Answer = int(First_Number) / int(Second_Number)
+    print("Answer: " + str(Answer))
+  Continue = input("Do you want to continue using this program? Yes or No? ")
+  if "Yes" in Continue:
+    Loop()
+  else:
+    print("Exiting...")
+    exit()
 def Loop():
   #Display of options
   print("0: Exit\n1: Addition\n2: Subtraction\n3: Multiplication\n4: Division")
-  #Selection of option & Rejection of input
+  #Selection of option
+  global Option
   Option = input("Pick the arithmetic operator that you wish to use (0,1,2,3,4): ")
   Option = int(Option)
-  #Processing of option
-  def Exit():
-    if Option == 0:
-      exit()
-  def Option_Addition():
-    if Option == 1:
-      First_Number = input("First number: ")
-      Second_Number = input("Second number: ")
-      Answer = int(First_Number) + int(Second_Number)
-      print("Answer: " + str(Answer))
-  def Option_Subtraction():
-    if Option == 2:
-      First_Number = input("First number: ")
-      Second_Number = input("Second number: ")
-      Answer = int(First_Number) - int(Second_Number)
-      print("Answer: " + str(Answer))
-  def Option_Multiplication():
-    if Option == 3:
-      First_Number = input("First number: ")
-      Second_Number = input("Second number: ")
-      Answer = int(First_Number) * int(Second_Number)
-      print("Answer: " + str(Answer))
-  def Option_Division():
-    if Option == 4:
-      First_Number = input("First number: ")
-      Second_Number = input("Second number: ")
-      Answer = int(First_Number) / int(Second_Number)
-      print("Answer: " + str(Answer))
-  Option_Addition()
-  Option_Subtraction()
-  Option_Multiplication()
-  Option_Division()
-  Continue = input("Do you want to continue using the program (Yes/No)? ")
-  if "Yes" in Continue:
-    Loop()
-  if "No" in Continue:
-    print("Exiting...")
+  #Start of process
+  Process()
 Loop()
